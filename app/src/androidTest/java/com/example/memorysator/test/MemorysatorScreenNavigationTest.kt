9b1e3197917bcc9398a33f6f66cbf696d54d6a25
Alexandre Bourse <Alexandre.bourse@gmail.com>
2024-02-02 16:49:20 -0500

    test: Configuring the NavHost

--- app/src/androidTest/java/com/example/memorysator/test/MemorysatorScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/memorysator/test/MemorysatorScreenNavigationTest.kt
@@ -1,4 +1,26 @@
 package com.example.memorysator.test
 
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.memorysator.ui.MemorysatorApp
+import org.junit.Before
+import org.junit.Rule
+
+@get:Rule
+val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+private lateinit var navController: TestNavHostController
+
+@Before
+fun setupMemorysatorNavHost(){
+    composeTestRule.setContent {
+        navController = TestNavHostController(LocalContext.current).apply {
+            navigatorProvider.addNavigator(ComposeNavigator())
+        }
+        MemorysatorApp() }
+}
+
 class MemorysatorScreenNavigationTest {
 }
