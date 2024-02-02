575010c7b8a036fb8389c996ba460dcdcd21b653
Alexandre Bourse <Alexandre.bourse@gmail.com>
2024-02-01 20:25:55 -0500

    feature : Added NavhHost and routes

--- app/src/main/java/com/example/memorysator/ui/MemorysatorApp.kt
+++ app/src/main/java/com/example/memorysator/ui/MemorysatorApp.kt
@@ -2,8 +2,16 @@ package com.example.memorysator.ui
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.memorysator.R
+import com.example.memorysator.ui.screen.DetailsScreen
+import com.example.memorysator.ui.screen.GameScreen
+import com.example.memorysator.ui.screen.MainMenuScreen
 
 
 enum class MemorysatorAppScreens(@StringRes val title: Int){
@@ -14,6 +22,24 @@ enum class MemorysatorAppScreens(@StringRes val title: Int){
 }
 
 @Composable
-fun MemorysatorApp() {
+fun MemorysatorApp(modifier: Modifier = Modifier,navController: NavHostController = rememberNavController()) {
+
+    NavHost(navController = navController, startDestination = MemorysatorAppScreens.MAIN_MENU.name){
+        composable(route= MemorysatorAppScreens.MAIN_MENU.name){
+            MainMenuScreen()
+        }
+
+        composable(route = MemorysatorAppScreens.DETAILS.name){
+            DetailsScreen()
+        }
+
+        composable(route = MemorysatorAppScreens.GAME.name){
+            GameScreen()
+        }
+
+        composable(route = MemorysatorAppScreens.DETAILS.name){
+            DetailsScreen()
+        }
+    }
 
 }
