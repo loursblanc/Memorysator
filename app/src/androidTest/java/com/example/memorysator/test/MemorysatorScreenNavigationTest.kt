24fb495a6d88e94316d1fde5aa4f3fbcbf09ef04
Alexandre Bourse <Alexandre.bourse@gmail.com>
2024-02-02 17:16:54 -0500

    test : Check the starting screen

--- app/src/androidTest/java/com/example/memorysator/test/MemorysatorScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/memorysator/test/MemorysatorScreenNavigationTest.kt
@@ -6,9 +6,11 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.example.memorysator.ui.MemorysatorApp
+import com.example.memorysator.ui.MemorysatorAppScreens
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
-
+import org.junit.Test
 
 
 class MemorysatorScreenNavigationTest {
@@ -26,5 +28,10 @@ class MemorysatorScreenNavigationTest {
             MemorysatorApp(navController = navController) }
     }
 
-    
+
+@Test
+fun memorysatorNavHost_verifyStartDestination(){
+    assertEquals(MemorysatorAppScreens.MAIN_MENU.name, navController.currentBackStackEntry?.destination?.route)
+}
+
 }
