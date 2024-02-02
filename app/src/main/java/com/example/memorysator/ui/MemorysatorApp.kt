3a367bad1155b2a29ee7c1896006ffa56e3588ad
Alexandre Bourse <Alexandre.bourse@gmail.com>
2024-02-01 20:11:30 -0500

    feature: Creating the enumeration for navigation
    - Added some dimensions in Resource

--- app/src/main/java/com/example/memorysator/ui/MemorysatorApp.kt
+++ app/src/main/java/com/example/memorysator/ui/MemorysatorApp.kt
@@ -1,7 +1,17 @@
 package com.example.memorysator.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
-import com.example.memorysator.ui.screen.GameScreen
+import androidx.navigation.compose.rememberNavController
+import com.example.memorysator.R
+
+
+enum class MemorysatorAppScreens(@StringRes val title: Int){
+    MAIN_MENU(title = R.string.game_title),
+    RULES(title = R.string.game_title),
+    GAME(title = R.string.game_title),
+    DETAILS(title = R.string.game_title),
+}
 
 @Composable
 fun MemorysatorApp() {
