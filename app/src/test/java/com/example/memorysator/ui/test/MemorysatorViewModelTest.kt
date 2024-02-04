03f6b85088b750ea43948fd34c84fff70c7a99b5
Alexandre Bourse <Alexandre.bourse@gmail.com>
2024-02-04 18:56:09 -0500

    test: currentPhoto is well modified in the UiState

--- app/src/test/java/com/example/memorysator/ui/test/MemorysatorViewModelTest.kt
+++ app/src/test/java/com/example/memorysator/ui/test/MemorysatorViewModelTest.kt
@@ -2,6 +2,7 @@ package com.example.memorysator.ui.test
 
 
 import com.example.memorysator.data.Difficulty
+import com.example.memorysator.network.Photo
 import com.example.memorysator.ui.screen.MemorysatorViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -13,7 +14,11 @@ class MemorysatorViewModelTest {
     @Test
     fun memorysatorViewModel_Initilization_DefaultMemorysatorUiState(){
         val memorysatorUiState = viewModel.uiState.value
-        assertEquals(Difficulty.EASY,memorysatorUiState.gameDifficulty)
+        val defaultDifficulty = Difficulty.EASY
+        val defaultCurrentPhoto = Photo("",0,"","","")
+        assertEquals(defaultDifficulty,memorysatorUiState.gameDifficulty)
+        assertEquals(defaultCurrentPhoto,memorysatorUiState.currentPhoto)
+        //assertEquals(defaultCurrentPhoto,memorysatorUiState.photos)
     }
 
     @Test
@@ -21,6 +26,14 @@ class MemorysatorViewModelTest {
         viewModel.setDifficulty(Difficulty.HARD)
         val memorysatorUiState = viewModel.uiState.value
         assertEquals(Difficulty.HARD,memorysatorUiState.gameDifficulty)
+    }
+
+    @Test
+    fun memorysatorVIewModel_ChangedCurrentPhoto_UiStateCurrentDifficultyChange(){
+        val photoTest = Photo ("Test",0,"test","image","none")
+        viewModel.setCurrentPhoto(photoTest)
+        val memorysatorUiState = viewModel.uiState.value
+        assertEquals(photoTest,memorysatorUiState.currentPhoto)
     }
 
 }
