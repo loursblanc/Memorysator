44757764d38267058b1360715a88d8b20ea9238d
Alexandre Bourse <Alexandre.bourse@gmail.com>
2024-02-05 17:09:24 -0500

    test: Added dependancy in gradle for Coroutines test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     testImplementation("junit:junit:4.13.2")
-
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
