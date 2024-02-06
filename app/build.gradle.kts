868b9b0cff5e3c56ae13535530cbe31d6a506e92
Alexandre Bourse <Alexandre.bourse@gmail.com>
2024-02-05 19:22:56 -0500

    feature: Added dependancy in gradle for Coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,9 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
